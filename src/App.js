b054f9d11e39364d1450f010c68f26b9df808072
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-05-06 17:41:01 -0400

    feat: tf classification implementee

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+// import * as mobilenet from '@tensorflow-models/mobilenet';
+
+// const img = document.getElementById('img');
+
+// // Load the model.
+// const model = await mobilenet.load();
+
+// // Classify the image.
+// const predictions = await model.classify(img);
+
+// console.log('Predictions: ');
+// console.log(predictions);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+      {/* <img id="img" crossorigin src="https://i.imgur.com/JlUvsxa.jpg" width="227" height="227"/> */}
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -17,7 +30,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const mobilenet = require('@tensorflow-models/mobilenet')
+global.fetch = require('node-fetch')
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +18,21 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// // -----> TF code labs demo
+let net;
+
+async function app() {
+  console.log('Loading mobilenet..');
+
+  // Load the model.
+  net = await mobilenet.load();
+  console.log('Successfully loaded model');
+
+  // Make a prediction through the model on our image.
+  const imgEl = document.getElementById('img');
+  const result = await net.classify(imgEl);
+  console.log(result);
+}
+
+app();
