bc8dc87528e415fca54d4cb5b6d2ac0247b36d6e
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-05-19 15:14:07 -0400

    add fb twitter share btns

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends React.Component {
     super();
     this.state = {
       word: "",
-      poem: null,
+      poem: null
     };
     this.updateWord = this.updateWord.bind(this);
     this.saveImage = this.saveImage.bind(this);
@@ -44,6 +44,7 @@ class App extends React.Component {
   render() {
     let { word, poem } = this.state;
     console.log("THIS STATE IN APP", this.state);
+    
     return (
       <div id="background">
         <div>
@@ -75,6 +76,13 @@ class App extends React.Component {
             null
           )}
           {!word.length ? <InstallButton /> : <div />}
+        </div>
+        <div id="share-btns">
+          {/* <div className="fb-share-button" data-href="https://haicoo.herokuapp.com/index.html" data-layout="button" data-size="large" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" target="_blank"> */}
+          <a className="fb-share-button" href="https://haicoo.herokuapp.com/index.html" data-layout="button" data-size="large" >Share</a>
+          {/* </div> */}
+          
+          <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-size="large" data-show-count="false">Twitter</a>
         </div>
       </div>
     );
