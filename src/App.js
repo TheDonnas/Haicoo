5f4ed7c29b901148081dc37ced9b122fadd2539c
yr.min17@gmail.com <Lily@aggieui-macbook.home>
2020-05-20 15:47:43 -0400

    share fb

--- src/App.js
+++ src/App.js
@@ -4,16 +4,19 @@ import Haiku from "./haiku";
 import * as htmlToImage from "html-to-image";
 import InstallButton from "./InstallButton";
 
+
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       word: "",
-      poem: null
+      poem: null,
+      copySuccess: ''
     };
     this.updateWord = this.updateWord.bind(this);
     this.saveImage = this.saveImage.bind(this);
     this.callbackFromHaiku = this.callbackFromHaiku.bind(this);
+    this.copyToClipboard = this.copyToClipboard.bind(this);
   }
 
   updateWord(word) {
@@ -30,7 +33,8 @@ class App extends React.Component {
           link.download = "haicoo.png";
           link.href = dataUrl;
           link.click();
-        });
+          localStorage.setItem('poemImg', dataUrl)
+        })
     } catch (err) {
       console.error(err);
     }
@@ -40,10 +44,21 @@ class App extends React.Component {
     this.setState({ poem: haiku });
     console.log("STATE POEM IN APP: ", this.state.poem);
   };
-
+  
+  copyToClipboard = () => {
+    var elem = document.createElement("textarea");
+    document.body.appendChild(elem);
+    elem.value = this.state.poem;
+    elem.select();
+    document.execCommand("copy");
+    document.body.removeChild(elem);
+  }
+  
   render() {
     let { word, poem } = this.state;
     console.log("THIS STATE IN APP", this.state);
+    let imgUrl = localStorage.getItem("poemImg")
+    document.getElementsByTagName('meta')[12].content= imgUrl
     
     return (
       <div id="background">
@@ -63,6 +78,29 @@ class App extends React.Component {
             callbackFromHaiku={this.callbackFromHaiku}
           />
         </div>
+        <div>
+        {this.state.poem &&
+          <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" type="button" onClick={this.copyToClipboard}><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"/></svg>
+        }
+        {
+         /* Logical shortcut for only displaying the 
+            button if the copy command exists */
+        //  document.queryCommandSupported('copy') &&
+        //   <div>
+        //     <button onClick={this.copyToClipboard}>Copy</button> 
+        //     {this.state.copySuccess}
+        //   </div>
+        }
+        {/* <form> */}
+          {/* {this.state.poem && 
+            <textarea
+            // ref={(textarea) => this.textArea = textarea}
+            value={this.state.poem}
+            />
+          } */}
+
+        {/* </form> */}
+      </div>
         <div>
           {word.length ? (
             <button
