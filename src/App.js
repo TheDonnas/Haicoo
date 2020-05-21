629141464581d4425670bd07f2b5a1a15e2e28bd
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2020-05-21 18:32:34 -0400

    deploy

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ class App extends React.Component {
     this.setState({ poem: haiku });
     console.log("STATE POEM IN APP: ", this.state.poem);
   };
-  
+
   copyToClipboard = () => {
     var elem = document.createElement("textarea");
     document.body.appendChild(elem);
@@ -65,26 +65,18 @@ class App extends React.Component {
     document.body.removeChild(elem);
   }
 
-<<<<<<< HEAD
 
-  render() {
-    let { word, poem, button } = this.state;
-    // console.log("THIS STATE IN APP", this.state);
-
-=======
-  
   render() {
     let { word, poem } = this.state;
     console.log("THIS STATE IN APP", this.state);
     // let imgUrl = localStorage.getItem("poemImg")
     // document.getElementsByTagName('meta')[12].content= imgUrl
-    
+
     let link = document.createElement('meta');
     link.setAttribute('property', 'og:image');
     link.content = localStorage.getItem("poemImg");
     document.getElementsByTagName('head')[0].appendChild(link);
-    
->>>>>>> 3d99b102ff6cd544cea095b26d998cd6c0511ea6
+
     return (
       <div id="background">
         <div id="home">
@@ -137,16 +129,16 @@ class App extends React.Component {
           <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24" type="button" onClick={this.copyToClipboard}><path d="M0 0h24v24H0z" fill="none"/><path d="M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm-1 4l6 6v10c0 1.1-.9 2-2 2H7.99C6.89 23 6 22.1 6 21l.01-14c0-1.1.89-2 1.99-2h7zm-1 7h5.5L14 6.5V12z"/></svg>
         }
         {
-         /* Logical shortcut for only displaying the 
+         /* Logical shortcut for only displaying the
             button if the copy command exists */
         //  document.queryCommandSupported('copy') &&
         //   <div>
-        //     <button onClick={this.copyToClipboard}>Copy</button> 
+        //     <button onClick={this.copyToClipboard}>Copy</button>
         //     {this.state.copySuccess}
         //   </div>
         }
         {/* <form> */}
-          {/* {this.state.poem && 
+          {/* {this.state.poem &&
             <textarea
             // ref={(textarea) => this.textArea = textarea}
             value={this.state.poem}
@@ -198,7 +190,7 @@ class App extends React.Component {
           {/* <div className="fb-share-button" data-href="https://haicoo.herokuapp.com/index.html" data-layout="button" data-size="large" href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse" className="fb-xfbml-parse-ignore" target="_blank"> */}
           <a className="fb-share-button" href="https://haicoo.herokuapp.com/index.html" data-layout="button" data-size="large">Share</a>
           {/* </div> */}
-          
+
           <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-size="large" data-show-count="false">Twitter</a>
 >>>>>>> 3d99b102ff6cd544cea095b26d998cd6c0511ea6
         </div>
